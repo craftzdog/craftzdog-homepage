dc06fbd76efcb8e17905e79657b278f651b4e36e
Takuya Matsuyama <hi@craftz.dog>
2021-10-13 20:48:06 +0900

    feat(navbar): add source link

--- components/navbar.js
+++ components/navbar.js
@@ -16,16 +16,19 @@ import {
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
 import ThemeToggleButton from './theme-toggle-button'
+import { IoLogoGithub } from 'react-icons/io5'
 
-const LinkItem = ({ href, path, children }) => {
+const LinkItem = ({ href, path, _target, children, ...props }) => {
   const active = path === href
   const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
   return (
-    <NextLink href={href}>
+    <NextLink href={href} passHref>
       <Link
         p={2}
         bg={active ? 'grassTeal' : undefined}
         color={active ? '#202023' : inactiveColor}
+        _target={_target}
+        {...props}
       >
         {children}
       </Link>
@@ -73,6 +76,18 @@ const Navbar = props => {
           </LinkItem>
           <LinkItem href="/posts" path={path}>
             Posts
+          </LinkItem>
+          <LinkItem
+            _target="_blank"
+            href="https://github.com/craftzdog/craftzdog-homepage"
+            path={path}
+            display="inline-flex"
+            alignItems="center"
+            style={{ gap: 4 }}
+            pl={2}
+          >
+            <IoLogoGithub />
+            Source
           </LinkItem>
         </Stack>
 
