9710da6e6bf7b1d50a877248700c96f17720d79d
Takuya Matsuyama <hi@craftz.dog>
2021-10-13 17:18:11 +0900

    fix(theme): typo

--- components/navbar.js
+++ components/navbar.js
@@ -24,7 +24,7 @@ const LinkItem = ({ href, path, children }) => {
     <NextLink href={href}>
       <Link
         p={2}
-        bg={active ? 'glassTeal' : undefined}
+        bg={active ? 'grassTeal' : undefined}
         color={active ? '#202023' : inactiveColor}
       >
         {children}
