96423dd468da00a3abb24c3d0857dc4307322c73
Takuya Matsuyama <hi@craftz.dog>
2021-11-05 11:54:49 +0900

    fix(navbar): browser prefixes are not added
    use `css` prop instaed. Ref: https://github.com/emotion-js/emotion/pull/1662

--- components/navbar.js
+++ components/navbar.js
@@ -45,7 +45,7 @@ const Navbar = props => {
       as="nav"
       w="100%"
       bg={useColorModeValue('#ffffff40', '#20202380')}
-      style={{ backdropFilter: 'blur(10px)' }}
+      css={{ backdropFilter: 'blur(10px)' }}
       zIndex={1}
       {...props}
     >
