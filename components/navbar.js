dec8f8afaaa1fa77d092dc8b2b24829023381aa0
YT Huang <38404369+yt-dev@users.noreply.github.com>
2022-02-20 01:40:05 +0800

    fix(navbar): unlegal prop

--- components/navbar.js
+++ components/navbar.js
@@ -27,7 +27,7 @@ const LinkItem = ({ href, path, _target, children, ...props }) => {
         p={2}
         bg={active ? 'grassTeal' : undefined}
         color={active ? '#202023' : inactiveColor}
-        _target={_target}
+        target={_target}
         {...props}
       >
         {children}
