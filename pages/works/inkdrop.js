866d4678f1c44670b1ae444c290c8f1882cda3b1
Takuya Matsuyama <hi@craftz.dog>
2022-05-10 21:47:32 +0900

    feat(inkdrop): add a video

--- pages/works/inkdrop.js
+++ pages/works/inkdrop.js
@@ -1,4 +1,11 @@
-import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
+import {
+  Container,
+  Badge,
+  Link,
+  List,
+  ListItem,
+  AspectRatio
+} from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, WorkImage, Meta } from '../../components/work'
 import P from '../../components/paragraph'
@@ -12,7 +19,7 @@ const Work = () => (
       </Title>
       <P>
         A Markdown note-taking app with 100+ plugins, cross-platform and
-        encrypted data sync support. The life-time revenue is more than $200k.
+        encrypted data sync support. The life-time revenue is more than $300k.
       </P>
       <List ml={4} my={4}>
         <ListItem>
@@ -40,6 +47,15 @@ const Work = () => (
 
       <WorkImage src="/images/works/inkdrop_01.png" alt="Inkdrop" />
       <WorkImage src="/images/works/inkdrop_02.png" alt="Inkdrop" />
+      <AspectRatio maxW="640px" ratio={1.7} my={4}>
+        <iframe
+          src="https://www.youtube.com/embed/-qBavwqc_mY"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </AspectRatio>
     </Container>
   </Layout>
 )
