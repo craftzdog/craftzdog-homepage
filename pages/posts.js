34fe18e31b0c99d0c890c6e5a8634a9fde9be2ea
Takuya Matsuyama <hi@craftz.dog>
2022-05-10 21:47:23 +0900

    feat(posts): add popular posts

--- pages/posts.js
+++ pages/posts.js
@@ -3,6 +3,8 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
 
+import thumbPortfolio from '../public/images/contents/youtube-how-to-build-portfolio.jpg'
+import thumbHowToUseInkdrop from '../public/images/contents/youtube-how-to-use-inkdrop.jpg'
 import thumbFishWorkflow from '../public/images/contents/youtube-fish-workflow.jpg'
 import thumbMyDeskSetup from '../public/images/contents/youtube-my-desk-setup.jpg'
 import thumb500PaidUsers from '../public/images/contents/blog-500-paid-users.jpg'
@@ -19,6 +21,16 @@ const Posts = () => (
 
       <Section delay={0.1}>
         <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            title="How to build a portfolio website"
+            thumbnail={thumbPortfolio}
+            href="https://www.youtube.com/watch?v=bSMZgXzC9AA"
+          />
+          <GridItem
+            title="How to take notes in Markdown efficiently with Inkdrop"
+            thumbnail={thumbHowToUseInkdrop}
+            href="https://www.youtube.com/watch?v=-qBavwqc_mY"
+          />
           <GridItem
             title="My Fish workflow"
             thumbnail={thumbFishWorkflow}
