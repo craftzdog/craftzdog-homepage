421ed2bd0cf5711416595e9a42c228998bb4def5
tylertaewook <70506321+tylertaewook@users.noreply.github.com>
2022-05-12 15:55:48 +0900

    changed name

--- components/layouts/article.js
+++ components/layouts/article.js
@@ -9,7 +9,7 @@ const variants = {
 }
 
 const Layout = ({ children, title }) => {
-  const t = `${title} - Takuya Matsuyama`
+  const t = `${title} - Tyler Kim`
   return (
     <motion.article
       initial="hidden"
