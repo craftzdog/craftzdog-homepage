ee9ab2592e4b9c0030783a2cbab86e5e93073d7a
JiantingFeng <13325471722@163.com>
2022-08-29 15:31:13 +0800

    added twitter summary card

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -18,10 +18,10 @@ const Main = ({ children, router }) => {
         <meta name="author" content="Jianting Feng" />
         <meta name="author" content="andyfeng" />
         <meta name="twitter:title" content="Jianting Feng" />
-        {/* <meta name="twitter:card" content="summary_large_image" /> */}
+        <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@jianting_feng" />
         <meta name="twitter:creator" content="@jianting_feng" />
-        {/* <meta name="twitter:image" content="https://www.craftz.dog/card.png" /> */}
+        <meta name="twitter:image" content="https://images.unsplash.com/photo-1660846316748-694183726acd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80" />
         <meta property="og:site_name" content="Jianting Feng" />
         <meta name="og:title" content="Jianting Feng" />
         <meta property="og:type" content="website" />
