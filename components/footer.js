a6aa3d7d53fa5c007d87c02cba7a42cd2d05f155
JiantingFeng <13325471722@163.com>
2022-09-01 12:31:11 +0800

    🍱 Change color mode links in footer

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,7 @@
 import { Box, Link, useColorModeValue } from '@chakra-ui/react'
 
 const Footer = () => {
+  const link_color = useColorModeValue('blue', 'red.200')
   return (
     <Box
       bg={useColorModeValue('#ffffff40', '#20202380')}
@@ -15,24 +16,15 @@ const Footer = () => {
     >
       &copy; {new Date().getFullYear()} Jianting Feng. All Rights Reserved.
       Build with {''}
-      <Link
-        textColor={useColorModeValue('blue', 'teal')}
-        href="https://nextjs.org"
-      >
+      <Link textColor={link_color} href="https://nextjs.org">
         Next.js
       </Link>{' '}
       and{' '}
-      <Link
-        textColor={useColorModeValue('blue', 'teal')}
-        href="https://chakra-ui.com"
-      >
+      <Link textColor={link_color} href="https://chakra-ui.com">
         ChakraUI
       </Link>
       , inspired by{' '}
-      <Link
-        textColor={useColorModeValue('blue', 'teal')}
-        href="https://www.craftz.dog"
-      >
+      <Link textColor={link_color} href="https://www.craftz.dog">
         Takuya Matsuyama
       </Link>
       .
