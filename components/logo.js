78d7df7dce8665b81689a742d1570df9341be7f9
Takuya Matsuyama <hi@craftz.dog>
2022-09-12 15:48:25 +0900

    fix(nav): replace footprint icon with svg image

--- components/logo.js
+++ components/logo.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
-import Image from 'next/image'
 import { Text, useColorModeValue } from '@chakra-ui/react'
+import FootprintIcon from './icons/footprint'
 import styled from '@emotion/styled'
 
 const LogoBox = styled.span`
@@ -12,23 +12,21 @@ const LogoBox = styled.span`
   line-height: 20px;
   padding: 10px;
 
-  img {
+  > svg {
     transition: 200ms ease;
   }
 
-  &:hover img {
+  &:hover > svg {
     transform: rotate(20deg);
   }
 `
 
 const Logo = () => {
-  const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`
-
   return (
     <Link href="/" scroll={false}>
       <a>
         <LogoBox>
-          <Image src={footPrintImg} width={20} height={20} alt="logo" />
+          <FootprintIcon />
           <Text
             color={useColorModeValue('gray.800', 'whiteAlpha.900')}
             fontFamily='M PLUS Rounded 1c", sans-serif'
