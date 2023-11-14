8b2c9b540d0ea8d09fc758cf2036ede908d73c82
Takuya Matsuyama <hi@craftz.dog>
2023-11-14 14:43:50 +0900

    feat(wallpapers): add pages and contents

--- components/grid-item.js
+++ components/grid-item.js
@@ -21,11 +21,17 @@ export const GridItem = ({ children, href, title, thumbnail }) => (
   </Box>
 )
 
-export const WorkGridItem = ({ children, id, title, thumbnail }) => (
+export const WorkGridItem = ({
+  children,
+  category = 'works',
+  id,
+  title,
+  thumbnail
+}) => (
   <Box w="100%" textAlign="center">
     <LinkBox
       as={NextLink}
-      href={`/works/${id}`}
+      href={`/${category}/${id}`}
       scroll={false}
       cursor="pointer"
     >
@@ -35,7 +41,7 @@ export const WorkGridItem = ({ children, id, title, thumbnail }) => (
         className="grid-item-thumbnail"
         placeholder="blur"
       />
-      <LinkOverlay as="div" href={`/works/${id}`}>
+      <LinkOverlay as="div" href={`/${category}/${id}`}>
         <Text mt={2} fontSize={20}>
           {title}
         </Text>
