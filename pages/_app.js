0a53c0cf9c1e99b83ac2026f7ff2f52601db1fb5
Takuya Matsuyama <hi@craftz.dog>
2023-11-14 22:39:57 +0900

    chore(analytics): add vercel's web analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Fonts from '../components/fonts'
 import { AnimatePresence } from 'framer-motion'
 import Chakra from '../components/chakra'
 import Payhip from '../components/payhip'
+import { Analytics } from '@vercel/analytics/react'
 
 if (typeof window !== 'undefined') {
   window.history.scrollRestoration = 'manual'
@@ -25,6 +26,7 @@ function Website({ Component, pageProps, router }) {
         >
           <Component {...pageProps} key={router.route} />
         </AnimatePresence>
+        <Analytics />
       </Layout>
     </Chakra>
   )
