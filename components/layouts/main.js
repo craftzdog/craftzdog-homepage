04cf8840213704aace9a7e8aa2f8f9721deb7118
Minimalist30 <minimalist0030@gmail.com>
2025-12-19 14:13:20 +0900

    fix: theee.js関連の実装を削除

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,14 +1,7 @@
-import Head from 'next/head'
-import dynamic from 'next/dynamic'
-import NavBar from '../navbar'
 import { Box, Container } from '@chakra-ui/react'
+import Head from 'next/head'
 import Footer from '../footer'
-import VoxelDogLoader from '../voxel-dog-loader'
-
-const LazyVoxelDog = dynamic(() => import('../voxel-dog'), {
-  ssr: false,
-  loading: () => <VoxelDogLoader />
-})
+import NavBar from '../navbar'
 
 const Main = ({ children, router }) => {
   return (
@@ -35,8 +28,6 @@ const Main = ({ children, router }) => {
       <NavBar path={router.asPath} />
 
       <Container maxW="container.md" pt={14}>
-        <LazyVoxelDog />
-
         {children}
 
         <Footer />
